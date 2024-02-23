c244acb40f9c43019a15459f0c66397beee99bd4
Pramod Vejendla <pramod.vejendla@gmail.com>
2024-02-24 01:38:02 +0530

    summary deleted

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,14 +1,14 @@
-import React, { Fragment } from 'react'
-import AvailableMeals from './AvailableMeals'
-import MealsSummary from './MealsSummary'
+import React, { Fragment } from "react";
+import AvailableMeals from "./AvailableMeals";
+import MealsSummary from "./MealsSummary";
 
 const Meals = () => {
   return (
     <Fragment>
-        <MealsSummary/>
-        <AvailableMeals />
+      {/* <MealsSummary/> */}
+      <AvailableMeals />
     </Fragment>
-  )
-}
+  );
+};
 
-export default Meals
+export default Meals;
